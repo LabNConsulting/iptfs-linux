5ec62cca97add1c1bb192734e57eaedf91cec983
Christian Hopps <chopps@gmail.com>
2022-10-31 15:20:21 +0000

    iptfs: impl: first packet sets expected seqno

--- net/xfrm/xfrm_iptfs.c
+++ net/xfrm/xfrm_iptfs.c
@@ -26,10 +26,10 @@
 /* #define IPTFS_ENET_OHEAD (14 + 4 + 8 + 12) */
 /* #define GE_PPS(ge, iptfs_ip_mtu) ((1e8 * 10 ^ (ge - 1) / 8) / (iptfs_ip_mtu)) */
 
-#define PR_DEBUG_INFO
-#define PR_DEBUG_STATE
+#undef PR_DEBUG_INFO
+#undef PR_DEBUG_STATE
 #undef PR_DEBUG_INGRESS
-#define PR_DEBUG_EGRESS
+#undef PR_DEBUG_EGRESS
 
 #ifdef PR_DEBUG_INFO
 #define _pr_devinf(...) pr_info(__VA_ARGS__)
@@ -70,7 +70,8 @@ struct xfrm_iptfs_data {
 	/*
 	 * Tunnel input reordering.
 	 */
-	u64 w_wantseq;		 /* expected next sequence */
+	bool w_seq_set;		  /* true after first seq received */
+	u64 w_wantseq;		  /* expected next sequence */
 	struct skb_wseq *w_saved; /* the saved buf array */
 	uint w_savedlen;	  /* the saved len (not size) */
 	struct spinlock drop_lock;
@@ -1345,9 +1346,17 @@ static uint iptfs_input_reorder(struct xfrm_iptfs_data *xtfs,
 {
 	const uint nslots = xtfs->cfg.reorder_win_size + 1;
 	u64 inseq = __esp_seq(inskb);
-	u64 wantseq = xtfs->w_wantseq;
+	u64 wantseq;
 
 	assert_spin_locked(&xtfs->drop_lock);
+
+	if (unlikely(!xtfs->w_seq_set)) {
+		pr_devinf("recv reorder: first packet inseq %llu skblen %u\n",
+			  inseq, inskb->len);
+		xtfs->w_seq_set = true;
+		xtfs->w_wantseq = inseq;
+	}
+	wantseq = xtfs->w_wantseq;
 
 	pr_devinf("recv reorder: inseq %llu want %llu savedlen %u skblen %u\n",
 		  inseq, wantseq, xtfs->w_savedlen, inskb->len);
